537be620a3e3ad34fbeddb9b797cd11748647b97
obideleon <robbydeleon@gmail.com>
2022-04-30 14:26:31 +0800

    Added error handler middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,19 @@
-const registerUser = (req, res) => {
-  res.send("Register Route");
-};
+const asyncHandler = require("express-async-handler");
 
-const loginUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please provide all fields.");
+  }
+
+  res.send("Register Route");
+});
+
+const loginUser = asyncHandler(async (req, res) => {
   res.send("Login Route");
-};
+});
 
 module.exports = {
   registerUser,
